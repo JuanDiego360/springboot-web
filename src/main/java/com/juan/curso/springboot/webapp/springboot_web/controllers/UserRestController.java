a6014bb5785b15ec79c32a8663d6670bc47464bb
JuanDiego360 <juandiego212014@gmail.com>
2026-01-28 17:02:11 -0500

    19. La Clase DTO

--- src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -8,15 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.juan.curso.springboot.webapp.springboot_web.models.User;
-
+import com.juan.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
-    
-    @GetMapping(path="/details")
-    public Map<String, Object> details() {
-        User user = new User("Juan","Florez");
+
+    @GetMapping(path = "/details")
+    public UserDto details() {
+
+        UserDto userDto = new UserDto();
+        User user = new User("Juan", "Florez");
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Sprint boot");
+
+        return userDto;
+    }
+
+    @GetMapping(path = "/details-map")
+    public Map<String, Object> detailsMap() {
+        User user = new User("Juan", "Florez");
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola Mundo Sprint boot");
