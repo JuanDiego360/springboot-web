0a6168d2b5b96d1ec20aa471731d02a95bcbb5a1
JuanDiego360 <juandiego212014@gmail.com>
2026-01-29 09:50:02 -0500

    25. Anotacion @ModelAttribute

--- src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.juan.curso.springboot.webapp.springboot_web.models.User;
 
@@ -27,16 +28,27 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(ModelMap model) {
+        // List<User> users = Arrays.asList(
+        // new User("Juan", "Florez"),
+        // new User("Maria", "Perez", "Maria123@correo.com"),
+        // new User("Pedro", "Gomez"),
+        // new User("Ana", "Lopez", "Ana123@correo.com"),
+        // new User("Luis", "Rodriguez", "Luis123@correo.com"));
+
+        model.addAttribute("title", "Listado de usuarios");
+        // model.addAttribute("users", users);
+        return "list"; // si modifico el return tengo que modificar el nombre del html en
+                       // resources/templates
+    }
+
+    @ModelAttribute("users")
+    public List<User> userModel() {
         List<User> users = Arrays.asList(
                 new User("Juan", "Florez"),
                 new User("Maria", "Perez", "Maria123@correo.com"),
                 new User("Pedro", "Gomez"),
                 new User("Ana", "Lopez", "Ana123@correo.com"),
                 new User("Luis", "Rodriguez", "Luis123@correo.com"));
-
-        model.addAttribute("title", "Listado de usuarios");
-        model.addAttribute("users", users);
-        return "list"; // si modifico el return tengo que modificar el nombre del html en
-                       // resources/templates
+        return users;
     }
 }
