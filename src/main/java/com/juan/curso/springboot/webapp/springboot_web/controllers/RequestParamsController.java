c8701adacdd1d28fd8bf88f0540bb9408798fb2d
JuanDiego360 <juandiego212014@gmail.com>
2026-01-29 11:20:10 -0500

    26. Anotacion @RequestParam

---
+++ src/main/java/com/juan/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -0,0 +1,25 @@
+package com.juan.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.juan.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+
+    @GetMapping("/foo")
+    public ParamDto foo(
+            @RequestParam(required = false, defaultValue = "Hola que tal?") String message) { // puedo cambiar el nombre
+                                                                                              // del parametro con name
+                                                                                              // y el valor por defecto
+                                                                                              // con defaultValue
+
+        ParamDto paramDto = new ParamDto();
+        paramDto.setMessage(message);
+        return paramDto;
+    }
+}
